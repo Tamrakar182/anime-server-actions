a7e6ed27d6ba574b3c05837d711a0b6eff9b9e39
Tamrakar182 <manjultamrakar4@gmail.com>
2023-12-17 18:00:25 +0545

    implement loading of cards

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,19 +1,33 @@
 "use client"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useInView } from "react-intersection-observer";
+import { fetchAnime } from "@/app/action";
+import { AnimeItem } from "@/types/Anime";
+import AnimeCard from "./AnimeCard";
+
+let page = 2;
 
 function LoadMore() {
   const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeItem[]>([]);
 
   useEffect(() => {
     if (inView) {
-      alert('inview');
+      fetchAnime(page).then((res) => {
+        setData([...data, ...res]);
+        page++;
+      })
     }
-  }, [inView])
+  }, [inView, data])
 
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeItem, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
           <Image
